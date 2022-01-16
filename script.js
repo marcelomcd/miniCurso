cd266a6d5d43df7b391bc2c3a7da3a34b584cf4b
marcelomcd <97555152+marcelomcd@users.noreply.github.com>
2022-01-16 16:28:44 -0300

    finalProject
    Arquivos Finais Para Envio

--- script.js
+++ script.js
@@ -1,28 +1,111 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const especieDoPersonagem = document.querySelector('#especie');
-const tipoDePersonagem = document.querySelector('#status');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
 
-gerarValorAleatorio = () => {
+const imagem2 = document.querySelector('img2');
+const botao2 = document.querySelector('bt2');
+const nomeDoPersonagem2 = document.querySelector('#nome2');
+const especie2 = document.querySelector('#especie2');
+const condicao2 = document.querySelector('#status2');
+
+const imagem3 = document.querySelector('img3');
+const botao3 = document.querySelector('button3');
+const nomeDoPersonagem3 = document.querySelector('#nome3');
+const especie3 = document.querySelector('#especie3');
+const condicao3 = document.querySelector('#status3');
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+traduzirCondicao2 = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+traduzirCondicao3 = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
+gerarValorAletorio = () => {
+    return Math.floor(Math.random() * 671);
+}
+gerarValorAletorio2 = () => {
+    return Math.floor(Math.random() * 671);
+}
+gerarValorAletorio3 = () => {
     return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
-    let numeroAleatorio = gerarValorAleatorio();
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`{
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
-            "Content-type": 'application/jason'
+            "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
-        especieDoPersonagem.innerHTML = data.species;
-        tipoDePersonagem.innerHTML = data.status;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduzirCondicao(data);
     });
+
 }
 
-botao.onclick = pegarPersonagem
+botao.onclick = pegarPersonagem;
+
+pegarPersonagem2 = () => {
+    let numeroAleatorio = gerarValorAletorio2();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        img2.src = data.image;
+        img2.alt = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
+        especie2.innerHTML = data.species;
+        condicao2.innerHTML = traduzirCondicao2(data);
+    });
+
+}
+
+pegarPersonagem3 = () => {
+    let numeroAleatorio = gerarValorAletorio3();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        img3.src = data.image;
+        img3.alt = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
+        especie3.innerHTML = data.species;
+        condicao3.innerHTML = traduzirCondicao3(data);
+    });
+
+}
